1f420da1ef87f31c3cc1b28000b4df4880755e88
Syed Muzaffar Rahaman <muzaffargamescartoons@gmail.com>
2025-06-20 21:44:45 +0530

    More Refactoring

--- entities/characters/validator.py
+++ entities/characters/validator.py
@@ -1,85 +1,94 @@
 from __future__ import annotations
 
-from enum import IntEnum
-
-from character import Character, CharacterSchema
+from character import Character
 from utils.graphs import toposort, build_reverse_graph
+from entities.entity import GameData, YamlSchema, SchemaValidationCode, EntityValidationCode
 
-CharacterSchema.loadAll()
-SchemasGraph = {name: schema.Extends for name, schema in CharacterSchema.registry.items()}
-Schemas = toposort(SchemasGraph)
-SchemasReverseGraph = build_reverse_graph(SchemasGraph)
+from pathlib import Path
+from utils.fields import HasField
+from utils.graphs import get_all_descendants
 
-
-class SchemaValidationCode(IntEnum):
-    UNACCEPTABLE = 0
-    INVALID = 1
-    VALID = 2
+from utils.fields import to_dict, flatten_fields
 
 
-class CharacterValidator:
-    @staticmethod
-    def ValidateSchema(char: Character, schema: CharacterSchema) -> tuple[SchemaValidationCode, set]:
+class CharacterSchema(YamlSchema):
+    files = GameData / Path('CharacterSchemas')
+
+    def ValidateMandatoryFields(self, char: Character):
         ValidatedFields = set()
-        MissingFields = set()
-        IsCoreSchema = False if not schema.Extends else True
-
-        from utils.fields import HasField
-
-        for field in schema.Mandatory:
+        for field in self.Mandatory:
             if HasField(char, field):
                 ValidatedFields.add(field)
             else:
-                MissingFields.add(field)
+                return None
+        return ValidatedFields
 
-        for field in schema.Optional:
-            if HasField(char, field): ValidatedFields.add(field)
+    def ValidateOptionalFields(self, char: Character):
+        ValidatedFields = set()
+        for field in self.Optional:
+            if HasField(char, field):
+                ValidatedFields.add(field)
+        return ValidatedFields
 
-        if not any(HasField(char, field) for field in schema.AnyOf):
-            MissingFields.update(schema.AnyOf)
+    def ValidateAnyOfFields(self, char: Character):
+        ValidatedFields = set()
+        for field in self.AnyOf:
+            if HasField(char, field):
+                ValidatedFields.add(field)
+        if len(ValidatedFields) == 0:
+            return None
         else:
-            for field in schema.AnyOf:
-                if HasField(char, field): ValidatedFields.add(field)
+            return ValidatedFields
 
-        if len(MissingFields) == 0:
-            ValidationCode = SchemaValidationCode.VALID
-        elif IsCoreSchema or len(ValidatedFields) != 0:
-            ValidationCode = SchemaValidationCode.UNACCEPTABLE
-        elif len(ValidatedFields) == 0:
-            ValidationCode = SchemaValidationCode.INVALID
-        else:
-            raise AssertionError("unreachable")
 
-        return ValidationCode, ValidatedFields
+    def ValidateSchema(self, char: Character) -> tuple[SchemaValidationCode, set]:
+        ValidatedFields = set()
 
-    @staticmethod
-    def ValidatePresenceOfFields(char: Character):
-        from utils.graphs import get_all_descendants
+        Mandatory = self.ValidateMandatoryFields(char)
+        AnyOf = self.ValidateAnyOfFields(char)
+        if Mandatory is None or AnyOf is None:
+            return SchemaValidationCode.SchemaNotImplemented, set()
+
+        ValidatedFields |= Mandatory
+        ValidatedFields |= self.ValidateOptionalFields(char)
+        ValidatedFields |= AnyOf
+
+        return SchemaValidationCode.SchemaImplemented, ValidatedFields
+
+
+    @classmethod
+    def Validate(cls, char: Character) -> tuple[EntityValidationCode, set, set, set]:
 
         DroppedSchemas = set()
-        ValidFields = set()
+        ImplementedSchemas = set()
 
-        ValidationSuccess = True
+        DefinedFields = set()
+
+        ValidationSuccess = EntityValidationCode.Valid
 
         for schema in Schemas:
             if schema in DroppedSchemas:
                 continue
 
-            Status, ValidatedFields = char.ValidateSchema(CharacterSchema.registry[schema])
+            Required = cls.registry[schema].Required
+            if not isinstance(Required, bool):
+                Required = any(ConditionalSchema in ImplementedSchemas for ConditionalSchema in Required)
 
-            if Status == SchemaValidationCode.VALID:
-                ValidFields |= ValidatedFields
-            elif Status == SchemaValidationCode.INVALID:
+            Status, ValidatedFields = cls.registry[schema].ValidateSchema(char)
+
+            if Status == SchemaValidationCode.SchemaImplemented:
+                DefinedFields |= ValidatedFields
+                ImplementedSchemas.add(schema)
+            elif not Required:
                 DroppedSchemas |= get_all_descendants(schema, SchemasReverseGraph)
-            elif Status == SchemaValidationCode.UNACCEPTABLE:
-                ValidationSuccess = False
+            else:
+                ValidationSuccess = EntityValidationCode.Invalid
 
-        return ValidationSuccess, ValidFields
+        UndefinedFields = flatten_fields(to_dict(char)) - DefinedFields
 
-    @staticmethod
-    def ValidateExtraneousFields(char: Character, ValidFields: set) -> set:
-        from utils.fields import to_dict, flatten_fields
+        return ValidationSuccess, DefinedFields, UndefinedFields, ImplementedSchemas
 
-        CharacterFields = flatten_fields(to_dict(char))
-        UndefinedFields = CharacterFields - ValidFields
-        return UndefinedFields
+CharacterSchema.loadAll()
+SchemasGraph = {name: schema.Extends for name, schema in CharacterSchema.registry.items()}
+Schemas = toposort(SchemasGraph)
+SchemasReverseGraph = build_reverse_graph(SchemasGraph)
--- entities/entity.py
+++ entities/entity.py
@@ -2,16 +2,33 @@ from pathlib import Path
 import yaml
 from utils.fields import to_dict
 
+GameData = Path('../.GameData')
+
+from enum import IntEnum
+class SchemaValidationCode(IntEnum):
+	SchemaImplemented = 0
+	SchemaNotImplemented = 1
+class EntityValidationCode(IntEnum):
+	Valid = 0
+	Invalid = 1
+
 
 class YamlEntity:
 	registry = {}
+
+	files: Path
 
 	def __init__(self, name: str):
 		self.Name = name
 		self._file_path = self.get_file_path(name)
 
-	def get_file_path(self, name: str) -> Path:
-		raise NotImplementedError("Subclasses must define get_file_path")
+	@classmethod
+	def get_file_path(cls, name: str) -> Path:
+		if cls.files is None:
+			raise NotImplementedError("Subclasses must define files: Path")
+		else:
+			cls.files.mkdir(exist_ok=True)
+			return cls.files / Path(name.replace(' ', '_') + '.yaml')
 
 	@classmethod
 	def create(cls, name: str, **data):
@@ -36,3 +53,11 @@ class YamlEntity:
 	def save(self):
 		with self._file_path.open('w') as file:
 			yaml.dump(to_dict(self), file, allow_unicode=True)
+
+class YamlSchema(YamlEntity):
+	@classmethod
+	def loadAll(cls):
+		for file in cls.files.glob('*.yaml'):
+			name = file.stem
+			if name not in cls.registry:
+				cls(name)
