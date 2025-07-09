21867d1bb2fcacbbcbf216fe8595b78015de4193
Syed Muzaffar Rahaman <muzaffargamescartoons@gmail.com>
2025-07-09 21:40:05 +0530

    Improved Attack Logic

--- entities/abilities.py
+++ entities/abilities.py
@@ -7,8 +7,13 @@ from entities.validator import YamlEntityValidator
 
 from main.config import GameData
 
+from utils.fields import GetField
+
 class Ability(YamlEntity):
 	files = GameData / Path('Entities') / Path('Abilities')
+
+	def Interpret(self):
+		return GetField(self, 'Damage'), GetField(self, 'DamageType')
 
 class AbilitySchema(YamlEntityValidator):
 	files = GameData / Path('Schemas') / Path('Abilities')
--- entities/character.py
+++ entities/character.py
@@ -14,9 +14,24 @@ from utils.fields import GetField, SetField
 class Character(YamlEntity):
 	files = GameData / Path('Entities') / Path('Characters')
 
-	def Attack(self, target: Character):
-		damage = CalculateModifier(GetField(self, 'Stats.Strength'))
-		SetField(target, 'Resource.HP.Current', damage, mode='-')
+	def Attack(self, target: Character, ability: str):
+		from entities.abilities import Ability
+		Ability.Load(ability)
+		AttackDamage, AttackDamageType = Ability.registry[ability].Interpret()
+
+		target.TakeDamage({AttackDamageType: AttackDamage})
+
+	def TakeDamage(self, DamageList: dict):
+		Resistances = GetField(self, 'Resistances')
+		Weaknesses = GetField(self, 'Weaknesses')
+
+		for damagetype in DamageList.keys():
+			DamageModifier = 1
+			if damagetype in Resistances.keys():
+				DamageModifier /= 1 + (Resistances[damagetype] / 100)
+			if damagetype in Weaknesses.keys():
+				DamageModifier *= 1 + (Weaknesses[damagetype] / 100)
+			SetField(self, 'Resource.HP.Current', DamageList[damagetype] * DamageModifier, mode='-')
 
 class CharacterSchema(YamlEntityValidator):
 	files = GameData / Path('Schemas') / Path('Characters')
